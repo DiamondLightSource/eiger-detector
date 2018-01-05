aa81655da3352b7fb24105405419a1ef3b7e6c37
Matthew Taylor <matthew.taylor@diamond.ac.uk>
2018-01-05 09:32:31 +0000

    Update to version 1-0-2

--- tools/python/setup.py
+++ tools/python/setup.py
@@ -35,7 +35,7 @@ def get_requirements():
   return required  
 
 setup(name='eiger_detector',
-      version="1.0.1",
+      version="1.0.2",
       description='Eiger Meta Data Filewriter',
       author='Matt Taylor',
       author_email='matthew.taylor@diamond.ac.uk',
