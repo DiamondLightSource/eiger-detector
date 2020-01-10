3a77873b0a31a2d4bd34689b6df3ecb5b3ea0d7e
Sky French <sky.french@diamond.ac.uk>
2020-01-10 08:21:25 +0000

    removing odindeploy from dls-master

--- odindeploy/templates/fp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$$( cd "$$( dirname "$$0" )" && pwd )"
-
-export HDF5_PLUGIN_PATH=${HDF5_FILTERS}
-
-${NUMA}$OD_ROOT/prefix/bin/frameProcessor --ctrl=tcp://0.0.0.0:$CTRL_PORT --ready=tcp://127.0.0.1:$READY_PORT --release=tcp://127.0.0.1:$RELEASE_PORT --json_file=$$SCRIPT_DIR/fp$NUMBER.json --logconfig $$SCRIPT_DIR/log4cxx.xml
--- odindeploy/templates/odin_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR="$$( cd "$$( dirname "$$0" )" && pwd )"
-
-$ODIN_SERVER --config=$$SCRIPT_DIR/$CONFIG --logging=error $EXTRA_PARAMS
