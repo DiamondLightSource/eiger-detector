c40aa8a7ddfae8bae65fa7b4daa3c76bdcbb7b54
Sky French <sky.french@diamond.ac.uk>
2020-02-26 09:28:57 +0000

    integration testing fixes

---
+++ .travis-install-hdf5filters.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+HDF5FILTERS_PREFIX=$HOME/hdf5filters
+git clone https://github.com/dls-controls/hdf5filters.git
+cd hdf5filters
+git checkout master
+mkdir -p cmake-build;
+cd cmake-build;
+cmake -DCMAKE_INSTALL_PREFIX=$HDF5FILTERS_PREFIX -DCMAKE_BUILD_TYPE=Release -DUSE_AVX2=ON -DBLOSC_ROOT_DIR=$HOME/c-blosc-1.14.2 -DHDF5_ROOT=$HOME/hdf5-1.10.1 ..
+make -j 8 VERBOSE=1
+make install
--- .travis-install-odin.sh
+++ .travis-install-odin.sh
@@ -9,3 +9,9 @@ cd build;
 cmake -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=ON -DCMAKE_INSTALL_PREFIX=$ODIN_PREFIX -DHDF5_ROOT=$HDF5_ROOT -DBLOSC_ROOT_DIR=$BLOSC_ROOT -DKAFKA_ROOT_DIR=$KAFKA_ROOT ..
 make -j 8 VERBOSE=1
 make install
+HDF5_DIR=$HDF5_ROOT pip install --user h5py
+echo $PWD
+cd ../
+cd tools/python
+python setup.py install --user
+cd ../../../
