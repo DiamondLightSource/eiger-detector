824eeb7f244bfdb57ef7445a4d57ede8c2304fc4
Gary Yendell <gary.yendell@diamond.ac.uk>
2020-06-09 16:35:31 +0100

    Add 9M sensor support

--- data/frameReceiver/include/EigerFrameDecoder.h
+++ data/frameReceiver/include/EigerFrameDecoder.h
@@ -88,6 +88,7 @@ namespace FrameReceiver
     static const std::string CONFIG_DETECTOR_MODEL;
     static const std::string DETECTOR_MODEL_500K;
     static const std::string DETECTOR_MODEL_4M;
+    static const std::string DETECTOR_MODEL_9M;
     static const std::string DETECTOR_MODEL_16M;
   };
 
