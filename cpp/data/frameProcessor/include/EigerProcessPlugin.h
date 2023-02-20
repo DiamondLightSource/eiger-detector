626ac283e456e7c12b7aeb5f9758ca1343f92ba0
Gary Yendell <gary.yendell@diamond.ac.uk>
2023-02-20 10:27:29 +0000

    Update process plugin for stream2
    Update to stream data directly into FP

--- cpp/data/frameProcessor/include/EigerProcessPlugin.h
+++ cpp/data/frameProcessor/include/EigerProcessPlugin.h
@@ -2,11 +2,13 @@
  * EigerProcessPlugin.h
  *
  *  Created on: 8 May 2017
- *      Author: Matt Taylor
+ *      Authors: Matt Taylor, Gary Yendell
  */
 
 #ifndef TOOLS_FILEWRITER_EIGERPROCESSPLUGIN_H_
 #define TOOLS_FILEWRITER_EIGERPROCESSPLUGIN_H_
+
+#include <stdint.h>
 
 #include <log4cxx/logger.h>
 #include <log4cxx/basicconfigurator.h>
@@ -15,20 +17,15 @@
 using namespace log4cxx;
 using namespace log4cxx::helpers;
 
+#include "zmq/zmq.hpp"
+#include "stream2.h"
+
 #include "FrameProcessorPlugin.h"
 #include "ClassLoader.h"
 #include "EigerDefinitions.h"
-#include <stdint.h>
 
 namespace FrameProcessor
 {
-  const int NO_COMPRESSION = 0;
-  const int LZ4_COMPRESSION = 1;
-  const int BSLZ4_COMPRESSION = 2;
-  const int UINT8_DATATYPE = 0;
-  const int UINT16_DATATYPE = 1;
-  const int UINT32_DATATYPE = 2;
-
   /** Processing of Eiger Frame objects.
    *
    * The EigerProcessPlugin class is responsible for receiving a raw data
@@ -48,12 +45,34 @@ namespace FrameProcessor
     std::string get_version_long();
 
   private:
+    void configure(OdinData::IpcMessage& config, OdinData::IpcMessage& reply);
+    void requestConfiguration(OdinData::IpcMessage& reply);
+
+    void handle_rx_socket();
+    void handle_start_msg(struct stream2_start_msg* message, const zmq::message_t& zmq_message);
+    void handle_image_msg(struct stream2_image_msg* message, boost::shared_ptr<Frame> frame);
+    void handle_end_msg(struct stream2_end_msg* message, const zmq::message_t& zmq_message);
     void process_frame(boost::shared_ptr<Frame> frame);
-    void setFrameEncoding(FrameMetaData &frame, const Eiger::FrameHeader* hdrPtr);
-    void setFrameDataType(FrameMetaData &frame, const Eiger::FrameHeader* hdrPtr);
-    void setFrameDimensions(FrameMetaData &frame, const Eiger::FrameHeader* hdrPtr);
+
+    DataType get_stream2_data_type(const struct stream2_image_data* image_data);
+    dimensions_t get_stream2_dimensions(const struct stream2_image_data* image_data);
+    CompressionType get_stream2_compression(const struct stream2_image_data* image_data);
+    uint32_t get_stream2_compressed_size(const struct stream2_image_data* image_data);
+
+    uint64_t current_series_id_;
+    std::string endpoint_;
+    zmq::context_t zmq_context_;
+    zmq::socket_t zmq_socket_;
+    boost::shared_ptr<boost::thread> rx_thread_;
+    /** Mutex used to make this class thread safe */
+    boost::recursive_mutex mutex_;
+    bool shutdown_;
+
     /** Pointer to logger */
     LoggerPtr logger_;
+
+    static const std::string CONFIG_ENDPOINT;
+
   };
 
   /**
